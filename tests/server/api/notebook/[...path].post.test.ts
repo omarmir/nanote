6c8fdfa116e98482f16e20152483af2043da5e23
Omar Mir <omarmir@gmail.com>
2025-03-10 01:35:50 -0400

    Implemented top level test

--- tests/server/api/notebook/[...path].post.test.ts
+++ tests/server/api/notebook/[...path].post.test.ts
@@ -1,20 +1,38 @@
 import { fileURLToPath } from 'node:url'
-import { describe, expect, it } from 'vitest'
-import { setup, $fetch, isDev } from '@nuxt/test-utils'
+import { beforeAll, describe, expect, it } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+import type { Notebook } from '~/types/notebook'
+import { join } from 'node:path'
+import basePath from '~/server/folder'
+import { getAuthCookie } from '~/tests/setup'
+import { access } from 'node:fs/promises'
 
-describe('example', async () => {
+let authCookie = ''
+describe('Notebook check', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('..', import.meta.url)),
     server: true
   })
 
-  it('Renders Hello Nuxt', async () => {
-    expect(await $fetch('/')).toMatch('Hello Nuxt!')
+  beforeAll(async () => {
+    authCookie = await getAuthCookie()
   })
 
-  if (isDev()) {
-    it('[dev] ensure vite client script is added', async () => {
-      expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
-    })
-  }
+  it('Response matches new notebook created', async () => {
+    const response = await $fetch('/api/notebook/Test', { method: 'POST', headers: { Cookie: authCookie } })
+    const resp: Notebook = {
+      notebooks: [],
+      name: 'Test',
+      createdAt: expect.any(String),
+      updatedAt: null,
+      notebookCount: 0,
+      noteCount: 0,
+      path: join(basePath, 'Test')
+    }
+    expect(response).toEqual(expect.objectContaining(resp))
+  })
+
+  it('Checks if folder was created', async () => {
+    await expect(access(join(basePath, 'Test'))).resolves.not.toThrow()
+  })
 })
