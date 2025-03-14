539363f29b92f50f33764555c37cc5adb26b6eb0
Omar Mir <omarmir@gmail.com>
2025-03-14 14:28:44 -0400

    Upload endpoint created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,23 +6,23 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: [
-    // '@nuxt/fonts',
+    '@nuxt/fonts',
     '@nuxt/eslint',
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     '@nuxt/test-utils/module'
   ],
-  // fonts: {
-  //   families: [
-  //     // do not resolve this font with any provider from `@nuxt/fonts`
-  //     { name: 'Rubik', provider: 'google', global: true },
-  //     // only resolve this font with the `google` provider
-  //     { name: 'Inter', provider: 'google', global: true },
-  //     // specify specific font data - this will bypass any providers
-  //     { name: 'JetBrains Mono', provider: 'google', global: true }
-  //   ]
-  // },
+  fonts: {
+    families: [
+      // do not resolve this font with any provider from `@nuxt/fonts`
+      { name: 'Rubik', provider: 'google', global: true },
+      // only resolve this font with the `google` provider
+      { name: 'Inter', provider: 'google', global: true },
+      // specify specific font data - this will bypass any providers
+      { name: 'JetBrains Mono', provider: 'google', global: true }
+    ]
+  },
   routeRules: {
     '/api/notebook': { redirect: '/api/notebook/*' }
   }
---
+++ server/api/attachment/index.post.ts
@@ -0,0 +1,93 @@
+import { customAlphabet } from 'nanoid'
+import { writeFile, mkdir, constants } from 'node:fs/promises'
+import path from 'node:path'
+import { access, existsSync } from 'node:fs'
+import { uploadPath } from '~/server/folder'
+// import { waitforme } from '~/server/utils'
+
+type MultiPartData = { name?: string; data: Buffer<ArrayBufferLike> | string; filename?: string }
+
+export default defineEventHandler(async (event): Promise<string> => {
+  try {
+    const formData = await readMultipartFormData(event)
+    if (!formData) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        message: 'Missing form data'
+      })
+    }
+
+    // await waitforme(5000)
+
+    // Find file in form data
+    // const fileEntry = formData.find((entry) => entry.name === 'file')
+    // const pathEntry = formData.find((entry) => entry.name === 'path')
+    const { fileEntry, pathEntry } = formData.reduce(
+      (acc, entry) => {
+        if (entry.name === 'file') acc.fileEntry = entry
+        if (entry.name === 'path') acc.pathEntry = entry
+        return acc
+      },
+      {} as { fileEntry?: MultiPartData; pathEntry?: MultiPartData }
+    )
+
+    if (!fileEntry?.data || !pathEntry?.data) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        message: 'No file uploaded or note path unspecified'
+      })
+    }
+
+    const attachBasePath = path.join(uploadPath, 'attachments')
+    if (!existsSync(attachBasePath)) {
+      await mkdir(attachBasePath, { recursive: true })
+    }
+
+    access(attachBasePath, constants.R_OK | constants.W_OK, (err) => {
+      if (err) {
+        console.log(err)
+        throw createError({
+          statusCode: 401,
+          statusMessage: 'Unauthorized',
+          message: 'Attachment folder is not read/write accessible.'
+        })
+      }
+    })
+
+    const nanoid = customAlphabet('abcdefghijklmop')
+    const id = nanoid()
+    const fileName = `${id}_${fileEntry.filename?.replace(/[\\/:*?"<>]/g, '')}`
+
+    if (fileName.length > 255) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        message: `Filename ${fileName} will exceed allowed length of 255 characters.`
+      })
+    }
+
+    const attachPath = path.join(attachBasePath, fileName)
+
+    const isWindows = process.platform === 'win32'
+    const maxPathLength = isWindows ? 259 : 4095 // Same limits as folder creation
+    if (attachPath.length > maxPathLength) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Bad Request',
+        message: 'Attachment file path is going to exceed maximum path length for your operating system.'
+      })
+    }
+
+    await writeFile(attachPath, fileEntry.data)
+    return `/api/attachment/${fileName}`
+  } catch (error) {
+    console.error('Error serving file:', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error',
+      message: `Failed to retrieve file: ${error}`
+    })
+  }
+})
