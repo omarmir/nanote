c21c7ecf37ae87a5cc1c5723d5ff8b099cd07f51
Omar Mir <omarmir@gmail.com>
2025-03-17 01:40:55 -0400

    Implemented marking attachments for deletion There is an off chance we could have two transactions come in at once and due to lack of atomicity we could result in improper marking.

--- server/api/note/[...path].patch.ts
+++ server/api/note/[...path].patch.ts
@@ -5,6 +5,7 @@ import type { NoteResponse } from '~/types/notebook'
 import { defineEventHandlerWithStorage } from '~/server/wrappers/storage'
 import type { UploadItem } from '~/types/upload'
 import type { StorageValue, Storage } from 'unstorage'
+import { notePathArrayJoiner } from '~/utils/path-joiner'
 
 const fileRegex = /::(file|fileBlock)\{href="(?<href>[^"]+)?"? title="(?<title>[^"]+)?"?\}/g
 
@@ -16,10 +17,23 @@ const markAttachmentForDeletionIfNeeded = async (
   notebook: string[],
   note: string,
   storage: Storage<StorageValue>,
-  fileData: Buffer<ArrayBufferLike>
+  fileData?: Buffer<ArrayBufferLike> | null
 ) => {
-  const uploads = await storage.getItem<UploadItem[]>('uploads')
-  console.log(uploads)
+  const uploads = (await storage.getItem<UploadItem[]>('uploads')) ?? []
+  const path = notePathArrayJoiner([...notebook, note])
+  const noteContent = (fileData ?? '').toString()
+
+  const matches = [...noteContent.matchAll(fileRegex)]
+    .map((match) => match.groups?.href.split('/').at(-1))
+    .filter((item) => item !== undefined)
+
+  // const unmatchedItems = uploads.filter((upload) => upload.path)
+  uploads.forEach((upload) => {
+    if (upload.path === path && !matches.includes(upload.path)) {
+      upload.deleted = true
+    }
+  })
+  await storage.setItem('uploads', uploads)
 }
 
 /**
@@ -59,7 +73,7 @@ export default defineEventHandlerWithStorage(async (event, storage): Promise<Not
       const newStats = await stat(fullPath)
 
       // Let the notes get marked for deletion
-      event.waitUntil(markAttachmentForDeletionIfNeeded(notebook, note, storage))
+      event.waitUntil(markAttachmentForDeletionIfNeeded(notebook, note, storage, fileEntry.data))
 
       return {
         notebook,
