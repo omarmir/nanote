134516f9f3fcde781c3cd53ae73bd19ad6865bba
Omar Mir <omarmir@gmail.com>
2025-04-10 23:28:17 -0400

    fix: Creating folders if they don't exist

--- server/folder.ts
+++ server/folder.ts
@@ -1,5 +1,5 @@
 import { join } from 'node:path'
-import { existsSync } from 'node:fs'
+import { existsSync, mkdirSync } from 'node:fs'
 
 // Define the default path (e.g., a "notes" folder in your project directory)
 const defaultNotesPath = join(process.cwd(), 'notes')
@@ -13,5 +13,19 @@ const envUploadsPath = process.env.UPLOAD_PATH
 // otherwise fall back to the default path.
 const notesPath = envNotesPath && existsSync(envNotesPath) ? envNotesPath : defaultNotesPath
 const uploadPath = envUploadsPath && existsSync(envUploadsPath) ? envUploadsPath : defaultUploadsPath
+
+try {
+  // Ensure directories exist
+  if (!existsSync(notesPath)) {
+    mkdirSync(notesPath, { recursive: true })
+  }
+
+  if (!existsSync(uploadPath)) {
+    mkdirSync(uploadPath, { recursive: true })
+  }
+} catch (error) {
+  console.log('Unable to create folders')
+  console.log(error)
+}
 
 export { notesPath, uploadPath, envNotesPath, envUploadsPath }
