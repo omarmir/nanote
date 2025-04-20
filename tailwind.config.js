89085e95917928e44c349eb5745f495c8cf41d59
Omar Mir <omarmir@gmail.com>
2025-04-19 21:21:42 -0400

    fix: css path compliation not including certain files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './pages/**/*.vue', './components/**/*.vue', './layouts/*.vue'],
   darkMode: 'class',
   theme: {
     extend: {
