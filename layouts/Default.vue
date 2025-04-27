963e679b4ccc68255cdd952399a789366eb09324
Omar Mir <omarmir@gmail.com>
2025-04-26 20:05:49 -0400

    fix: default layout fix to account for the removal of the second sidebar feat: increased width of the sidebar since it no longer slides out to a second one

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <SlidingSidebar></SlidingSidebar>
-    <div
-      id="panel"
-      class="relative h-full max-h-screen rounded-xl transition-all duration-200 lg:ml-[300px]"
-      :class="{ 'lg:ml-[600px]': store.currentNotebook }">
+    <div id="panel" class="relative h-full max-h-screen rounded-xl transition-all duration-200 lg:ml-[350px]">
       <TopBar class="px-5 lg:px-24"></TopBar>
       <div class="mx-auto w-full px-5 py-6 lg:px-24">
         <slot></slot>
@@ -12,6 +9,4 @@
     </div>
   </div>
 </template>
-<script lang="ts" setup>
-const store = useNotebookStore()
-</script>
+<script lang="ts" setup></script>
