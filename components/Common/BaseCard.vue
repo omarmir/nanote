0c9b8e7925ca8336766c9163059b99db23d74b86
Omar Mir <omarmir@gmail.com>
2025-04-29 00:45:13 -0400

    fix: main cards take up more space on mobile closes #31

--- components/Common/BaseCard.vue
+++ components/Common/BaseCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative mb-5 flex min-w-0 flex-col break-words rounded-md border border-gray-200 bg-white bg-clip-border shadow-sm dark:border-neutral-700 dark:bg-neutral-900">
+    class="relative mb-5 flex min-w-0 flex-col break-words border-b border-t border-gray-200 bg-white bg-clip-border shadow-sm dark:border-neutral-700 dark:bg-neutral-900 md:rounded-md md:border">
     <slot></slot>
   </div>
 </template>
