46cf1947b7af728bbb151abb5954ca24a1f32195
Omar Mir <omarmir@gmail.com>
2025-06-04 11:08:20 -0400

    refactor: pdf gen to be more reusable

--- server/api/note/download/pdf/[...path].get.ts
+++ server/api/note/download/pdf/[...path].get.ts
@@ -2,103 +2,19 @@
 import { readFileSync } from 'node:fs'
 import contentDisposition from 'content-disposition'
 import { defineEventHandlerWithNotebookAndNote } from '~/server/wrappers/note'
-import { convertMarkdownToHtml } from '~/server/utils/html-gen'
+import { convertMarkdownToHtml, imageRegex, printPDF, replaceFileContent } from '~/server/utils/html-gen'
 import { blockRegex, regex as inlineRegex } from 'milkdown-plugin-file/regex'
-import puppeteer from 'puppeteer'
-import { getIcon } from 'material-file-icons'
-import { settings } from '~/server/db/schema'
-import jwt from 'jsonwebtoken'
-import SECRET_KEY from '~/server/key'
-
-const printPDF = async (html: string, origin: string, hostname: string) => {
-  const token = jwt.sign({ app: 'nanote' }, SECRET_KEY, { expiresIn: '7d' })
-
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-  // Create a new browser context
-  const context = await browser.createBrowserContext()
-  context.setCookie({
-    httpOnly: true,
-    name: 'token',
-    value: token,
-    domain: hostname,
-    sameSite: 'Strict',
-    expires: Math.floor(Date.now() / 1000) + 60 * 5, // expires in 5 minutes
-    path: '/'
-  })
-
-  try {
-    const page = await context.newPage()
-    await page.goto(origin)
-
-    await page.setContent(html, { waitUntil: 'networkidle0' })
-    await page.addStyleTag({
-      content: `@media print {
-        @page {
-          margin: 1cm 1.5cm; /* top-bottom, left-right */
-        }
-        html {
-          font-family: sans-serif;
-        }
-
-        img {
-          max-width: 100%;
-        }
-
-        body {
-          /* Optional: avoid extra spacing issues */
-          margin: 0;
-        }
-      }`
-    })
-
-    const paraSpacing = await db.query.settings.findFirst({
-      where: eq(settings.setting, 'isParagraphSpaced')
-    })
-    if (paraSpacing?.value === 'true') {
-      await page.addStyleTag({ content: 'p {padding-bottom: 0.25rem}' })
-    }
-
-    const pdf = await page.pdf({ format: 'A4' })
-
-    await browser.close()
-    return pdf
-  } catch (error) {
-    console.log(error)
-    await browser.close()
-    throw new Error(`PDF generation failed: ${error instanceof Error ? error.message : String(error)}`)
-  }
-}
-
-const replaceFileContent = (htmlContent: string, regex: RegExp) => {
-  htmlContent = htmlContent.replace(regex, (match, _hrefGroup, _titleGroup, _offset, _originalString, groups) => {
-    // The `groups` object (the last argument) contains the named capture groups.
-    // groups.title should contain the content of the title attribute.
-    const title = groups?.title ?? 'N/A'
-    const icon = getIcon(title)
-    if (title) {
-      return `
-            <span style="display: inline-flex; flex-direction: row; align-items: center; gap: 0.25rem;">
-            ${regex === blockRegex ? 'File: ' : ''}[<span style="display: inline-block; width: 1rem; height: 1rem;">${icon.svg}</span> ${title.trim()}]
-            </span>
-            ` // .trim() to remove any potential leading/trailing spaces within the quotes
-    }
-    return match // Fallback: if title is not captured for some reason, return the original match.
-  })
-  return htmlContent
-}
 
 export default defineEventHandlerWithNotebookAndNote(async (event, _cleanNotebook, cleanNote, fullPath) => {
   const { origin, host } = getRequestURL(event)
   // const nanoid = customAlphabet('abcdefghijklmnop')
 
   const content = readFileSync(fullPath, 'utf8')
-  const urlRegex = /(?<=\(<|\()\/api\/attachment\/.*?(?=[)>])/g
-  let newContent = ''
-  newContent = content.replace(urlRegex, (matchedUrl) => `${origin}${matchedUrl}`)
+  const newContent = content.replace(imageRegex, (matchedUrl) => `${origin}${matchedUrl}`)
 
   let htmlContent: string = await convertMarkdownToHtml(newContent)
-  htmlContent = replaceFileContent(htmlContent, blockRegex)
-  htmlContent = replaceFileContent(htmlContent, inlineRegex)
+  htmlContent = replaceFileContent(htmlContent, true, blockRegex)
+  htmlContent = replaceFileContent(htmlContent, false, inlineRegex)
 
   const pdf = await printPDF(htmlContent, origin, host)
 
