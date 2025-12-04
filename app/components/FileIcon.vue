fb6558acd9e74cbfae90a476d31c0b9c9e078bf2
Omar Mir <omarmir@gmail.com>
2025-12-04 15:39:58 -0500

    wip

--- app/components/FileIcon.vue
+++ app/components/FileIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-html="getIcon(name ?? '').svg" v-if="!isMarkdown" class="size-5 shrink-0"></div>
-  <UIcon name="i-custom-ri-markdown-line" class="text-primary size-5 shrink-0" v-else></UIcon>
+  <UIcon name="i-custom-ri-markdown-line" class="text-neutral size-5 shrink-0" v-else></UIcon>
 </template>
 
 <script lang="ts" setup>
