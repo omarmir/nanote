66563185978e705f8d4f6c5043aa4b07c45c742b
Omar Mir <omarmir@gmail.com>
2025-12-05 20:26:32 -0500

    Add GitHub icon and enhance button styling in the theme

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,12 @@
           <UDashboardSidebarCollapse />
         </template>
         <template #right>
-          <ThemePicker></ThemePicker>
+          <div class="flex flex-row items-center gap-4">
+            <ThemePicker class="size-5 cursor-pointer"></ThemePicker>
+            <ULink to="https://github.com/omarmir/nanote" target="_blank">
+              <UAvatar icon="i-custom-simple-icons-github" size="md" />
+            </ULink>
+          </div>
         </template>
       </UDashboardNavbar>
     </template>
