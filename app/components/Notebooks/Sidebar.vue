b42d9a86b2d9ff78cca8a2f221b77be006bd3e89
Omar Mir <omarmir@gmail.com>
2025-12-05 21:31:29 -0500

    Enhance Sidebar component with additional button for dense mode toggle and refactor button layout

--- app/components/Notebooks/Sidebar.vue
+++ app/components/Notebooks/Sidebar.vue
@@ -2,14 +2,20 @@
   <div v-if="!collapsed">
     <div class="mb-2 flex flex-row items-center justify-between">
       <h2 class="text-sm font-bold">{{ t('Notebook', 2) }}</h2>
-      <UButton
-        size="sm"
-        variant="ghost"
-        color="warning"
-        icon="i-lucide-fold-vertical"
-        class="cursor-pointer"
-        :class="{ invisible: expanded.length === 0 }"
-        @click="expanded = []"></UButton>
+      <div class="flex flex-row" :class="{ invisible: expanded.length === 0 }">
+        <UButton
+          size="sm"
+          variant="ghost"
+          color="warning"
+          icon="i-lucide-fold-vertical"
+          class="cursor-pointer"
+          @click="expanded = []"></UButton>
+        <UButton
+          :icon="settingsStore.settings.isDense ? 'i-custom-code-more' : 'i-custom-code-less'"
+          variant="ghost"
+          class="cursor-pointer"
+          @click="settingsStore.toggleDenseMode()"></UButton>
+      </div>
     </div>
 
     <UTree
@@ -46,6 +52,7 @@ import type { TreeItemToggleEvent } from 'reka-ui'
 
 const { t } = useI18n()
 const notebookStore = useNotebookStore()
+const settingsStore = useSettingsStore()
 
 const expanded = ref([])
 
