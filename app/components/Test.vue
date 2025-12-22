4ea37339a2411535195329feb73a257ba2b09fa8
Omar Mir <omarmir@gmail.com>
2025-12-22 14:20:25 -0500

    Test accessibility file

---
+++ app/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <UButton
+    size="md"
+    :icon="isReadOnly ? 'i-lucide-pencil' : 'i-lucide-pencil-off'"
+    color="neutral"
+    square
+    :ui="{ leadingIcon: 'text-primary' }" />
+</template>
+<script lang="ts" setup>
+const isReadOnly = useState('isReadOnly', () => false)
+</script>
