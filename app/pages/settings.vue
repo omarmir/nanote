bf0186b2b604647ca19fc0ce2aacc0342533528c
Omar Mir <omarmir@gmail.com>
2025-12-23 12:15:22 -0500

    partially implemented settings and switch

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -1,45 +1,7 @@
-<script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
-
-const links = [
-  [
-    {
-      label: 'General',
-      icon: 'i-lucide-user',
-      to: '/settings',
-      exact: true
-    },
-    {
-      label: 'Members',
-      icon: 'i-lucide-users',
-      to: '/settings/members'
-    },
-    {
-      label: 'Notifications',
-      icon: 'i-lucide-bell',
-      to: '/settings/notifications'
-    },
-    {
-      label: 'Security',
-      icon: 'i-lucide-shield',
-      to: '/settings/security'
-    }
-  ],
-  [
-    {
-      label: 'Documentation',
-      icon: 'i-lucide-book-open',
-      to: 'https://ui.nuxt.com/docs/getting-started/installation/nuxt',
-      target: '_blank'
-    }
-  ]
-] satisfies NavigationMenuItem[][]
-</script>
-
 <template>
-  <UDashboardPanel id="settings" :ui="{ body: 'lg:py-12' }">
+  <UDashboardPanel id="settings">
     <template #header>
-      <UDashboardNavbar title="Settings">
+      <UDashboardNavbar :title="t('navigation.settings')">
         <template #leading>
           <UDashboardSidebarCollapse />
         </template>
@@ -52,9 +14,40 @@ const links = [
     </template>
 
     <template #body>
-      <div class="mx-auto flex w-full flex-col gap-4 sm:gap-6 lg:max-w-2xl lg:gap-12">
-        <NuxtPage />
-      </div>
+      <NuxtPage />
     </template>
   </UDashboardPanel>
 </template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+const { t } = useI18n()
+
+const links = [
+  [
+    {
+      label: t('settings.navigation.general'),
+      icon: 'i-lucide-settings-2',
+      to: '/settings',
+      exact: true
+    },
+    {
+      label: t('settings.navigation.shared'),
+      icon: 'i-lucide-share-2',
+      to: '/settings/shared'
+    },
+    {
+      label: t('settings.navigation.health'),
+      icon: 'i-lucide-activity',
+      to: '/settings/health'
+    }
+  ],
+  [
+    {
+      label: t('navigation.guide'),
+      icon: 'i-lucide-circle-question-mark',
+      to: '/guide'
+    }
+  ]
+] satisfies NavigationMenuItem[][]
+</script>
